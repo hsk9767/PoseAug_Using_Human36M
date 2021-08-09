a174ef956ba74958b7375679fdc0eea80240c524
Hyunsung Kim <34028332+hsk9767@users.noreply.github.com>
2021-08-09 14:56:03 +0900

    Add files via upload

---
+++ hrnet_simplebaseline/lib/models/__init__.py
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .pose_resnet import get_pose_net
+from .pose_hrnet import get_pose_net
